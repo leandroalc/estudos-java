0fd614ef7a03a18ace460aa223027e3ff75d4cb9
Leandro Alcoforado <leandrofalcao@live.com>
2022-05-15 01:59:21 -0300

    program created in IntelliJ

---
+++ src/br/com/leandroalc/HelloWorld.java
@@ -0,0 +1,15 @@
+package br.com.leandroalc;
+
+import br.com.leandroalc.model.Gato;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        /*int a = 5;
+        int b = 3;
+        System.out.println("Hello World " + (a+b));*/
+
+        Gato gato = new Gato();
+
+        System.out.println(gato);
+    }
+}
